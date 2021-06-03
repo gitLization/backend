3657fca38797a5df29ae162b935d814a0a8ad165
DESKTOP-CG7I5KV\younjkim <objection@kakao.com>
2021-06-03 23:09:19 +0900

    quiz demo

---
+++ quiz_game.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>분리수거 퀴즈</title>
+		
+	</head>
+	<body>
+	
+		<div id="container">
+		<h1>분리수거 퀴즈</h1>
+		
+		<form method="" action="<?php echo base_url();?>index.php/Questions/quizdisplay">
